fd855383bcd431dfb5fa5d9ebe4ab3f680f273f3
Tarek Campos <tarekcampossaleh@gmail.com>
2025-06-25 09:16:26 -0300

    Add markdown rendering

--- init.lua
+++ init.lua
@@ -264,7 +264,18 @@ require('lazy').setup({
       })
     end,
   },
-
+  -- Mini-vim
+  { 'echasnovski/mini.nvim', version = false },
+  -- Markdown rendering
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
   -- Formatting
   {
     'stevearc/conform.nvim',
